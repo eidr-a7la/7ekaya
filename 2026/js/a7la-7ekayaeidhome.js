ae61e0ddc23809a7f0a743b21f76868a522149bd
ahmedto <ahmed.to2004@gmail.com>
2026-05-04 16:05:51 +0300

    Update a7la-7ekayaeidhome.js

--- 2026/js/a7la-7ekayaeidhome.js
+++ 2026/js/a7la-7ekayaeidhome.js
@@ -120,17 +120,21 @@ jQuery(document).ready(function($) {
         e.stopPropagation(); // منع انتقال الضغطة للشاشة
         
         var $wrapper = $(this).closest('.a7la-sub-dropdown-wrapper');
+        var $card = $(this).closest('.a7la7ekaya-forum-card'); // استهداف البطاقة الرئيسية
         
-        // إغلاق أي قوائم أخرى مفتوحة
+        // إغلاق أي قوائم أخرى مفتوحة في بطاقات أخرى
         $('.a7la-sub-dropdown-wrapper').not($wrapper).removeClass('is-active');
+        $('.a7la7ekaya-forum-card').not($card).removeClass('has-active-dropdown');
         
-        // فتح أو إغلاق القائمة الحالية
+        // فتح أو إغلاق القائمة الحالية ورفع بطاقتها
         $wrapper.toggleClass('is-active');
+        $card.toggleClass('has-active-dropdown');
     });
 
     // إغلاق القائمة عند الضغط في أي مكان فارغ في الشاشة
     $(document).on('click', function() {
         $('.a7la-sub-dropdown-wrapper').removeClass('is-active');
+        $('.a7la7ekaya-forum-card').removeClass('has-active-dropdown'); // إرجاع البطاقة لمكانها
     });
 
     // منع الإغلاق عند الضغط بداخل القائمة نفسها
